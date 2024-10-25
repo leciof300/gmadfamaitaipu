150d3840de778f9e977fc9fc8184e58d410b0cd7
leciof300 <lecioferreiraguimaraes208@gmail.com>
2024-10-25 08:21:42 -0300

    Atualizando o site com novos arquivos

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'membros-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Arquivos em cache adicionados');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
